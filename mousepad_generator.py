c4428e100903f07cc9c52cfc27a5b605118ca90d
Cadu Quaglia <cadusquaglia@gmail.com>
2024-09-24 09:43:39 -0300

    Second commit

--- mousepad_generator.py
+++ mousepad_generator.py
@@ -5,84 +5,84 @@ import json
 import os
 
 
-class Generator():
-    def __init__(self):
-        pass
-    
+class Generator():    
     def add_logo(self):
-        '''
         # Logo to image ratio
-        ratio = 0.25
-
-        # Logo width position factor
-        pos_w = .98
+        ratio = self.data["config"]["logo_proportion"]
 
         # Logo height position factor
-        pos_h = .95
+        pos_h = self.data["config"]["logo_vertical"]
+
+        # Logo width position factor
+        pos_w = self.data["config"]["logo_horizontal"]
+
 
         # CODE
-        factor = ratio/ (logo.width/img.width)
-        logo = ImageOps.scale(image=logo, factor=factor)
-        offset = (int((img.width - logo.width) / pos_w), int((img.height - logo.height) / pos_h))
+        factor = ratio/ (self.logo.width/self.img.width)
+        self.logo = ImageOps.scale(image=self.logo, factor=factor)
+        offset = (int((self.img.width - self.logo.width) / pos_w), int((self.img.height - self.logo.height) / pos_h))
         
-        img.paste(logo, offset, logo)
-    
-        return self.img
-        '''
-        pass
+        self.img.paste(self.logo, offset, self.logo)
+        
 
     def add_border(self):
-        '''
-        # Border radio
-        rad = 100
-
         # Border width
-        border_w = 35
+        border_w = int(self.data["config"]["border_width"])
+        
+        # Border radio
+        rad = int(self.data["config"]["border_radius"])
+
         
         # CODE
         # Add rounded border
-        img = img.convert("RGBA")
+        self.img = self.img.convert("RGBA")
 
-        new = Image.new('RGBA', img.size, (255, 255, 255, 0))
+        new = Image.new('RGBA', self.img.size, (255, 255, 255, 0))
         draw = D.Draw(new)
 
-        draw.rounded_rectangle([(0, 0), (img.width, img.height)], fill=(255, 0, 0, 0), outline="black",
+        draw.rounded_rectangle([(0, 0), (self.img.width, self.img.height)], fill=(255, 0, 0, 0), outline="black",
                             width=border_w, radius=rad)
         
-        img = Image.alpha_composite(img, new)
+        self.img = Image.alpha_composite(self.img, new)
 
         # Cut final image
         circle = Image.new('L', (rad * 2, rad * 2), 0)
         draw = D.Draw(circle)
         draw.ellipse((0, 0, rad * 2 - 1, rad * 2 - 1), fill=255)
-        alpha = Image.new('L', img.size, 255)
-        w, h = img.size
+        alpha = Image.new('L', self.img.size, 255)
+        w, h = self.img.size
         alpha.paste(circle.crop((0, 0, rad, rad)), (0, 0))
         alpha.paste(circle.crop((0, rad, rad, rad * 2)), (0, h - rad))
         alpha.paste(circle.crop((rad, 0, rad * 2, rad)), (w - rad, 0))
         alpha.paste(circle.crop((rad, rad, rad * 2, rad * 2)), (w - rad, h - rad))
-        img.putalpha(alpha)
-
-        out = img
-        return out
-        '''
-        pass
-
-    def save_product():
-        #add_logo()
-        pass
+        self.img.putalpha(alpha)
 
     
-    def save_display():
-        #add_logo()
-        #add_border()
-        pass
+    def show_images(self):
+        img_name = self.data["paths"]["img_source_filename"]
+        logo_name = self.data["paths"]["logo_source_filename"]
 
-    def table_main():
-        pass
+        img_id, extension = img_name.split('.')
 
-    def table_config():
-        pass
+        self.img =  Image.open(f"{self.data["paths"]["img_source_folder"] + "\\" + img_name}", "r")
+        self.logo = Image.open(f"{self.data["paths"]["logo_source_folder"] + "\\" + logo_name}", "r")
+        
+        self.add_logo()
+        self.img_product = self.img
+
+        self.add_border()
+        self.img_site = self.img
+
+        self.save_img_site = self.data["paths"]["img_site_folder"] + "\\" + "site_" + img_id + ".png"
+        self.save_img_product = self.data["paths"]["img_products_folder"] + "\\" + "produto_" + img_id + ".png"
+        
+        #self.img_product.show()
+        self.img_site.show()
+
+
+    def save_images(self):
+        self.img_site.save(self.save_img_site)
+        self.img_product.save(self.save_img_product)
 
     
     @property
@@ -114,21 +114,32 @@ def main():
     data['config']['border_radius'] = 10
     gen.data = data
     """
+    data = gen.data
 
     menu_main = me.Menu(["GERAR IMAGENS", "ALTERAR imagem ({<filename>})", "ALTERAR cor da logo (<filename>)", "CONFIGURAÇÕES"], 
-                        "Mousepad Generator\n\n", exitStr="SAIR")   
+                        "Mousepad Generator\n\n", exitStr="SAIR")
     
     selection_main = 1
 
     while selection_main != 0:
+        menu_main.choice_strings = [f"GERAR IMAGENS", 
+                                  f"ALTERAR imagem ({data["paths"]["img_source_filename"]})", 
+                                  f"ALTERAR cor da logo ({data["paths"]["logo_source_filename"]})", 
+                                  f"CONFIGURAÇÕES"]
         clear()
         selection_main = menu_main.selection()
 
         match selection_main:
             case 1:
-                # Gerar imagens
-                pass
-            
+                gen.show_images()
+
+                save = '1'
+                while save != 'y' and save != 'n':
+                    save = input("Salvar imagens? [y/n]: ")
+                
+                if save == 'y':
+                    gen.save_images()
+
             case 2:
                 # Alterar imagem fonte
                 img_source_new = input("Nova imagem: ")
@@ -170,7 +181,7 @@ def main():
 
                     if selection_config != 0:
                         config_key = config_list[selection_config - 1]
-                        data["config"][config_key] = int(input(f"Atual: {data["config"][config_key]} | Novo: "))
+                        data["config"][config_key] = float(input(f"Atual: {data["config"][config_key]} | Novo: "))
                         gen.data = data
 
 
